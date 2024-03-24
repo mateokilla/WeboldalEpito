0df3038ab85df6009f975d0111ecc19c4a07989a
mateokilla <nitro_5_bernath_mark@outlook.hu>
2024-03-24 18:07:19 +0100

    JS alapok ismétlés

---
+++ index.js
@@ -0,0 +1,11 @@
+function ValamitCSinal() {
+  console.log("Ez egy függvény!");
+}
+//ValamitCSinal();
+
+let üzenet = "Üzenet egy változóban!";
+
+function ÜzenetVáltozóMEgjelenítő(valami) {
+  console.log(valami);
+}
+ÜzenetVáltozóMEgjelenítő(üzenet);
